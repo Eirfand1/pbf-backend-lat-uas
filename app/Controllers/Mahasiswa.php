ecad0a2ec9098f2fe0af17c0d6f30a66ffa8a720
abdau88 <abdau@pnc.ac.id>
2025-05-16 13:06:48 +0700

    Save local changes before rebase

---
+++ app/Controllers/Mahasiswa.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Mahasiswa extends ResourceController
+{
+      protected $modelName = 'App\Models\MahasiswaModel';
+    protected $format    = 'json';
+    /**
+     * Return an array of resource objects, themselves in array format.
+     *
+     * @return ResponseInterface
+     */
+    public function index()
+    {
+           $data = $this->model
+            ->select('mahasiswa.*, dosen.nama as dosen_wali')
+            ->join('dosen', 'dosen.id = mahasiswa.dosen_wali_id')
+            ->findAll();
+
+        return $this->respond($data);
+    }
+
+    /**
+     * Return the properties of a resource object.
+     *
+     * @param int|string|null $id
+     *
+     * @return ResponseInterface
+     */
+    public function show($id = null)
+    {
+        //
+    }
+
+    /**
+     * Return a new resource object, with default properties.
+     *
+     * @return ResponseInterface
+     */
+    public function new()
+    {
+        //
+    }
+
+    /**
+     * Create a new resource object, from "posted" parameters.
+     *
+     * @return ResponseInterface
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Return the editable properties of a resource object.
+     *
+     * @param int|string|null $id
+     *
+     * @return ResponseInterface
+     */
+    public function edit($id = null)
+    {
+        //
+    }
+
+    /**
+     * Add or update a model resource, from "posted" properties.
+     *
+     * @param int|string|null $id
+     *
+     * @return ResponseInterface
+     */
+    public function update($id = null)
+    {
+        //
+    }
+
+    /**
+     * Delete the designated resource object from the model.
+     *
+     * @param int|string|null $id
+     *
+     * @return ResponseInterface
+     */
+    public function delete($id = null)
+    {
+        //
+    }
+}
